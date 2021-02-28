ac8912199654963095fd4ac9415e23c849357264
juliaborisovva <juliaborisovva@mail.ru>
2021-02-28 21:01:16 +0700

    Rename FOUND/NOTFOUND to FAILURE/SUCCESS, error to implementation + Type, Bool type introduction

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 
 enum LengthsOfFigures { LENGTH_OF_CIRCLE = 6, LENGTH_OF_TRIANGLE = 8 };
 
-enum TypesOfFigures { UNKNOWN, CIRCLE, TRIANGLE };
+typedef enum TypesOfFigures { UNKNOWN, CIRCLE, TRIANGLE } Type;
 
-enum ErrorStatus { NOTFOUND = 0, FOUND = -1 };
+typedef enum ErrorStatus { SUCCESS = 0, FAILURE = -1 } Bool;
 
 void skip_space(char** cursor_start)
 {
@@ -16,18 +16,18 @@ void skip_space(char** cursor_start)
     }
 }
 
-int check_punctuation_symbols(char** cursor_start, /*int* error,*/ char symbol)
+Bool check_punctuation_symbols(char** cursor_start, /*int* implementation,*/ char symbol)
 {
     if (**cursor_start == symbol) {
         (*cursor_start)++;
-        return NOTFOUND;
+        return SUCCESS;
     } else {
         printf("Error: expected \"%c\"\n\n", symbol);
-        return FOUND;
+        return FAILURE;
     }
 }
 
-int check_extra_token(char** cursor_start/*, int* error*/)
+Bool check_extra_token(char** cursor_start/*, int* implementation*/)
 {
     while (**cursor_start != '\0') {
         if ((isalnum(**cursor_start) != 0) || (ispunct(**cursor_start) != 0)) {
@@ -35,12 +35,12 @@ int check_extra_token(char** cursor_start/*, int* error*/)
                    "nothing after "
                    "data string\n\n");
             **cursor_start = '\0';
-            return FOUND;
+            return FAILURE;
         } else {
             (*cursor_start)++;
         }
     }
-    return NOTFOUND;
+    return SUCCESS;
 }
 
 /*если встречается буква, то пока она встречается
@@ -58,7 +58,7 @@ void select_type (char** cursor_start, char** cursor_end, char* input) {
 /*Если введенный тип совпал с известными нам типами фигур, то печатаем
 тип и выставляем переменной figure соответствующее значение, иначе -
 вывести ошибку*/
-int determine_figure (char** cursor_start, int* error, size_t length_of_type, char* type_circle, char* type_triangle) {
+Type determine_figure (char** cursor_start, /*int* implementation, */size_t length_of_type, char* type_circle, char* type_triangle) {
     if (strncasecmp(*cursor_start, type_circle, LENGTH_OF_CIRCLE) == 0
         && length_of_type == LENGTH_OF_CIRCLE) {
         return CIRCLE;
@@ -68,51 +68,51 @@ int determine_figure (char** cursor_start, int* error, size_t length_of_type, ch
         return TRIANGLE;
     } else {
         printf("Error: expected \"circle\" | \"triangle\"\n\n");
-        *error = FOUND;
+        //*implementation = FAILURE;
         return UNKNOWN;
     }
 }
 
 
-int parse_circle(char **cursor_start, char **cursor_end, int* counter) {
-    int error2;
+Bool parse_circle(char **cursor_start, char **cursor_end, int* counter) {
+    Bool implementation2;
     float x1, y1, radius1;
     x1 = strtof(*cursor_start, &(*cursor_end));
     if (*cursor_start == *cursor_end) {
         printf("Error: expected float x1\n\n");
-        return FOUND;
+        return FAILURE;
     }
     *cursor_start = *cursor_end;
     y1 = strtof(*cursor_start, &(*cursor_end));
     if (*cursor_start == *cursor_end) {
         printf("Error: expected float y1\n\n");
-        return FOUND;
+        return FAILURE;
     }
     *cursor_start = *cursor_end;
     skip_space(&(*cursor_start));
-    error2 = check_punctuation_symbols(&(*cursor_start),/*&(*error),*/ ',');
-    if (error2 == FOUND) {
-        return FOUND;
+    implementation2 = check_punctuation_symbols(&(*cursor_start),/*&(*implementation),*/ ',');
+    if (implementation2 == FAILURE) {
+        return FAILURE;
     }
     radius1 = strtof(*cursor_start, &(*cursor_end));
     if (*cursor_start == *cursor_end) {
         printf("Error: expected float radius\n\n");
-        return FOUND;
+        return FAILURE;
     }
     *cursor_start = *cursor_end;
     skip_space(&(*cursor_start));
-    error2 = check_punctuation_symbols(&(*cursor_start),/*&(*error),*/ ')');
-    if (error2 == FOUND) {
-        return FOUND;
+    implementation2 = check_punctuation_symbols(&(*cursor_start),/*&(*implementation),*/ ')');
+    if (implementation2 == FAILURE) {
+        return FAILURE;
     }
-    error2 = check_extra_token(&(*cursor_start)/*, &(*error)*/);
-    if (error2 == FOUND) {
-        return FOUND;
+    implementation2 = check_extra_token(&(*cursor_start)/*, &(*implementation)*/);
+    if (implementation2 == FAILURE) {
+        return FAILURE;
     }
     (*counter)++;
     printf("%d Тип фигуры: circle\n", *counter);
     printf("x1 = %.1lf y1 = %.1lf \nradius1 = %.1lf\n \n", x1, y1, radius1);
-    return NOTFOUND;
+    return SUCCESS;
 }
 
 
@@ -126,8 +126,8 @@ int main()
     size_t length_of_type = 0;
     char type_circle[] = {"circle"};
     char type_triangle[] = {"triangle"};
-    int figure;
-    int error;
+    Type figure;
+    Bool implementation;
     int counter = 0;
     //для кейса треугольника:
     //float x2, y2, x3, y3, x4, y4 = 0;
@@ -135,7 +135,7 @@ int main()
     printf("Напишите то, что хотите проанализировать:\n");
     while ((fgets(input, 70, stdin))) {
         figure = UNKNOWN;
-        error = NOTFOUND;
+        implementation = SUCCESS;
         fputs(input, stdout); // если надо вывести введенные данные
         cursor_start = input;
         cursor_end = input;
@@ -143,18 +143,18 @@ int main()
 
         select_type(&cursor_start, &cursor_end, input);
         length_of_type = cursor_end - cursor_start;
-        figure = determine_figure(&cursor_start, &error, length_of_type, type_circle, type_triangle);
-        if (error == FOUND) {
+        figure = determine_figure(&cursor_start, /*&implementation, */length_of_type, type_circle, type_triangle);
+        if (figure == UNKNOWN) {
             continue;
         }
 
         cursor_start = cursor_end;
         skip_space(&cursor_start);
-        error = check_punctuation_symbols(&cursor_start, /*&error, */'(');
-        if (figure == TRIANGLE && error == NOTFOUND) {
-            error = check_punctuation_symbols(&cursor_start, /*&error, */'(');
+        implementation = check_punctuation_symbols(&cursor_start, /*&implementation, */'(');
+        if (figure == TRIANGLE && implementation == SUCCESS) {
+            implementation = check_punctuation_symbols(&cursor_start, /*&implementation, */'(');
         }
-        if (error == FOUND) {
+        if (implementation == FAILURE) {
             continue;
         }
 
@@ -162,15 +162,15 @@ int main()
         switch (figure) {
             case CIRCLE:
                 parse_circle(&cursor_start, &cursor_end, &counter);
-                /*if (error == FOUND) {
-                    printf("\n%d ОШИБКА НАЙДЕНА\n\n", error);
+                /*if (implementation == FAILURE) {
+                    printf("\n%d ОШИБКА НАЙДЕНА\n\n", implementation);
                 } else {
-                    printf("\n%d ОШИБКА НЕ НАЙДЕНА\n\n", error);
+                    printf("\n%d ОШИБКА НЕ НАЙДЕНА\n\n", implementation);
                 }*/
                 break;
 
-            /*case TRIANGLE:
-                x1 = strtof(cursor_start, &cursor_end);
+            case TRIANGLE:
+                /*x1 = strtof(cursor_start, &cursor_end);
                 if (cursor_start == cursor_end) {
                     printf("Error: expected float x1\n\n");
                     continue;
@@ -183,8 +183,8 @@ int main()
                 }
                 cursor_start = cursor_end;
                 skip_space(&cursor_start);
-                check_punctuation_symbols(&cursor_start, &error, ',');
-                if (error == FOUND) {
+                check_punctuation_symbols(&cursor_start, &implementation, ',');
+                if (implementation == FAILURE) {
                     continue;
                 }
 
@@ -201,8 +201,8 @@ int main()
                 }
                 cursor_start = cursor_end;
                 skip_space(&cursor_start);
-                check_punctuation_symbols(&cursor_start, &error, ',');
-                if (error == FOUND) {
+                check_punctuation_symbols(&cursor_start, &implementation, ',');
+                if (implementation == FAILURE) {
                     continue;
                 }
 
@@ -219,8 +219,8 @@ int main()
                 }
                 cursor_start = cursor_end;
                 skip_space(&cursor_start);
-                check_punctuation_symbols(&cursor_start, &error, ',');
-                if (error == FOUND) {
+                check_punctuation_symbols(&cursor_start, &implementation, ',');
+                if (implementation == FAILURE) {
                     continue;
                 }
 
@@ -237,16 +237,16 @@ int main()
                 }
                 cursor_start = cursor_end;
                 skip_space(&cursor_start);
-                check_punctuation_symbols(&cursor_start, &error, ')');
-                if (error == FOUND) {
+                check_punctuation_symbols(&cursor_start, &implementation, ')');
+                if (implementation == FAILURE) {
                     continue;
                 }
-                check_punctuation_symbols(&cursor_start, &error, ')');
-                if (error == FOUND) {
+                check_punctuation_symbols(&cursor_start, &implementation, ')');
+                if (implementation == FAILURE) {
                     continue;
                 }
-                check_extra_token(&cursor_start, &error);
-                if (error == FOUND) {
+                check_extra_token(&cursor_start, &implementation);
+                if (implementation == FAILURE) {
                     continue;
                 }
                 //передать данные о переменных,
@@ -264,8 +264,8 @@ int main()
                        x3,
                        y3,
                        x4,
-                       y4);
-                break;*/
+                       y4);*/
+                break;
         }
     }
     return 0;
